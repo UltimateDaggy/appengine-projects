22e6f4e6a50e8f170770b446bad1f94bac287da5
Faraz Dastagir <faraz.dastagir@hotmail.com>
2012-07-26 00:04:44 +0100

    Added login code to app_controller.. not right though; still giving server error
    To Ahmed: I've added '#' before the parts that I thought weren't needed
    from what you told me , and added some question comments in the
    login/logout code

--- app/controller/app_controller.py
+++ app/controller/app_controller.py
@@ -15,6 +15,30 @@ class AppController(webapp2.RequestHandler):
         # return it to the browser
         self.response.out.write(template)
 
+#class MainPage(webapp2.RequestHandler):
+    def get(self):
+#        guestbook_name=self.request.get('guestbook_name')
+#        greetings_query = Greeting.all().ancestor(
+#            guestbook_key(guestbook_name)).order('-date')
+#        greetings = greetings_query.fetch(10)
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+#        template_values = {
+#            'greetings': greetings,
+#            'url': url,
+#            'url_linktext': url_linktext,
+#        }
+
+#        template = jinja_environment.get_template('index.html')
+            #the templates being 'get' below... is that where i need to add this code? 
+#        self.response.out.write(template.render(template_values))
+            #if i'm not using template_values then this isn't needed, right?        
 
 def get_template(data):
     # prepare our templating engine
