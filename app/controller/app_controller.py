448804b80a70154ced897bafc75f2c5e22b435e3
Faraz Dastagir <faraz.dastagir@hotmail.com>
2012-07-28 16:39:13 +0100

    Login button & temporarily a seperate logout button added

--- app/controller/app_controller.py
+++ app/controller/app_controller.py
@@ -4,41 +4,31 @@ import jinja2
 import webapp2
 
 from app.model import comment_model
+from google.appengine.api import users
 
 class AppController(webapp2.RequestHandler):
     def get(self):
         # get our template
         template = get_template({
-            'comments' : comment_model.get_comments()
+            'comments' : comment_model.get_comments(),
+            #google account stuff
+            'display_user_account' : users.get_current_user(),
+            'url_link_text' : 'log-in',
+            'url_account_action' : users.create_login_url(self.request.uri),
+            #temporary logout button
+            'logout_link_text' : 'temp.log-out',
+            'temp_logout' : users.create_logout_url(self.request.uri),
+
+            #if users.get_current_user():
+            #url = users.create_logout_url(self.request.uri)
+            #url_linktext = 'Logout'
+            #else:
+            #url = users.create_login_url(self.request.uri)
+            #url_linktext = 'Login'
         })
 
         # return it to the browser
         self.response.out.write(template)
-
-#class MainPage(webapp2.RequestHandler):
-    def get(self):
-#        guestbook_name=self.request.get('guestbook_name')
-#        greetings_query = Greeting.all().ancestor(
-#            guestbook_key(guestbook_name)).order('-date')
-#        greetings = greetings_query.fetch(10)
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-#        template_values = {
-#            'greetings': greetings,
-#            'url': url,
-#            'url_linktext': url_linktext,
-#        }
-
-#        template = jinja_environment.get_template('index.html')
-            #the templates being 'get' below... is that where i need to add this code? 
-#        self.response.out.write(template.render(template_values))
-            #if i'm not using template_values then this isn't needed, right?        
 
 def get_template(data):
     # prepare our templating engine
