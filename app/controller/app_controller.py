6f846c7e9c720d7477570a001d9c00d553bb5484
Faraz Dastagir <faraz.dastagir@hotmail.com>
2012-08-02 14:52:14 +0100

    Sorted Login/Logout button into one

--- app/controller/app_controller.py
+++ app/controller/app_controller.py
@@ -14,18 +14,26 @@ class AppController(webapp2.RequestHandler):
         if account_user == None:
             account_user = ''
 
+        #Specifying Login/Logout button
+        account_action = users.get_current_user()
+
+        if account_action == None:
+            account_action_text = 'Login'
+            account_action_url = users.create_login_url(self.request.uri)
+        else:
+            account_action_text = 'Logout'
+            account_action_url = users.create_logout_url(self.request.uri)
+
         # get our template
         template = get_template({
             'comments' : comment_model.get_comments(),
+
             #google account stuff
             'display_user_account' : account_user,
             
-            #login button
-            'url_link_text' : 'log-in',
-            'url_account_action' : users.create_login_url(self.request.uri),
-            #temporary logout button
-            'logout_link_text' : 'log-out',
-            'temp_logout' : users.create_logout_url(self.request.uri),
+            #login/Logout button
+            'url_link_text' : account_action_text,
+            'url_account_action' : account_action_url,
             })
 
         # return it to the browser
